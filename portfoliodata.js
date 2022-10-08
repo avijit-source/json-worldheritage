a1206c819a191355de9201bd0c9d9b3d992de896
avijit-source <79460627+avijit-source@users.noreply.github.com>
2022-10-08 22:47:24 +0530

    Create portfoliodata.js

---
+++ portfoliodata.js
@@ -0,0 +1,2 @@
+"cvLink":"https://github.com/avijit-source/json-worldheritage/raw/gh-pages/Resume_08_10_2022_10_42_07_PM.pdf",
+"about":"The idea of reaching the North Pole by means of balloons appears to have been entertained many years ago."
